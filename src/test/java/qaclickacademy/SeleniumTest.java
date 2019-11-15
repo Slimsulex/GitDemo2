b2f02fda61d29f3b8c36ffc7f099ac0827c31d18
Olufemi <opemutairu@gmail.com>
2019-11-14 21:00:53 -0500

    New Update from Asian guy

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -10,12 +10,16 @@ public class SeleniumTest {
         System.out.println("I am Browser Automation UI code");
         
         System.out.println("I am Web Automation code");
+        System.out.println("I am Bariga code");
+        System.out.println("I am Marietta code");
 	}
 	
 	@Test
 	public void RadioButton()
 	{
 		System.out.println("I am Radio Button code");
+		System.out.println("I am video button code");
+		System.out.println("I am Radio code");
 	}
 	
 	
